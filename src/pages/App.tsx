bce0f16d9fa3845510e7158fc175c8384fc0a11a
张晓亮 <zhangxl@sqbj.com>
2019-02-28 14:39:36 +0800

    project init

---
+++ src/pages/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Header from "./layout/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import PageA from "./test/PageA";
+import PageB from "./test/PageB";
+
+const routes = [{ path: "/", component: PageA }, { path: "/test/page-a", component: PageA }, { path: "/test/page-b", component: PageB }];
+
+class App extends Component {
+	render() {
+		return (
+			<Router basename="apps">
+				<div id="app">
+					<ul>
+						<li>
+							<Link to="/test/page-a">PageA</Link>
+						</li>
+						<li>
+							<Link to="/test/page-b">PageB</Link>
+						</li>
+					</ul>
+					<hr />
+					<Header />
+					<Switch>
+						{routes.map(route => (
+							<Route exact={true} path={route.path} component={route.component as any} key={route.path} />
+						))}
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+}
+
+export default App;
---
+++ src/pages/test/PageA.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Button } from "antd";
+
+export default class extends React.Component {
+	public render() {
+		return (
+			<div>
+				page-a
+				<Button type="primary">antd-button</Button>
+			</div>
+		);
+	}
+}
